65424925daca5a058f818a79891ff1557f187740
nathanclairmonte <nathanclairmonte@outlook.com>
2023-03-23 23:48:22 -0400

    fixed typing issues w predictions assertions

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -14,8 +14,8 @@ def test_make_prediction(sample_input_data):
     # then
     predictions = result.get("predictions") # using .get here in case key doesn't exist
     print(len(predictions))
-    assert isinstance(predictions, list)
-    assert all(isinstance(x, np.float64) for x in predictions)
+    assert isinstance(predictions, np.ndarray)
+    assert all(isinstance(x, np.int64) for x in predictions)
     assert result.get("errors") is None
     # assert len(predictions) == expected_num_preds
     # assert math.isclose(predictions[0], expected_first_pred_value, abs_tol=100)
