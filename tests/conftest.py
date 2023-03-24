212f2d2f380692bfb29db53994ee93f9da122101
nathanclairmonte <nathanclairmonte@outlook.com>
2023-03-23 23:47:49 -0400

    fixed typo in sample data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from classification_model.config.core import config
 from classification_model.processing.data_manager import load_dataset
 
 @pytest.fixture()
-def sample_imput_data():
+def sample_input_data():
     return load_dataset(file_name=config.app_config.test_data_file)
